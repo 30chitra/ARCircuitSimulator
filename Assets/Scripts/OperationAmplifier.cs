0c9a5d02dc690e7de6e867dbcd6aab39f59901c8
Amit Chawla <43717429+Amit173@users.noreply.github.com>
2021-02-21 03:33:51 +0530

    Update OperationAmplifier.cs

--- Assets/Scripts/OperationAmplifier.cs
+++ Assets/Scripts/OperationAmplifier.cs
@@ -7,7 +7,7 @@ using TMPro;
 
 public class OperationAmplifier : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
 
     private GameObject GraphBackground, showGraphButton;
     private GameObject InputWave, OutputWave;
